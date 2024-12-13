c6ce1a24dd65be47284f999fc02f7ad348c07a7f
AriSyafri <diamondThink15@gmail.com>
2024-12-14 01:04:57 +0700

    mengganti pemanggilan bootrap dari vendor menjadi di public

--- app/View/header.php
+++ app/View/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title><?= $model['title'] ?? 'toko atk' ?></title>
 
-    <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
     
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
