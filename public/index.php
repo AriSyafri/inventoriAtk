9ebb860e981e263a9eee758cd42e0d818e0ba19c
AriSyafri <diamondThink15@gmail.com>
2024-12-13 23:53:18 +0700

    menghapus dari namespacefile sebelumnya

--- public/index.php
+++ public/index.php
@@ -5,15 +5,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dots\Toko\Atk\App\Router;
 use Dots\Toko\Atk\Controller\HomeController;
-use Dots\Toko\Atk\Controller\ProductController;
-use Dots\Toko\Atk\Middleware\AuthMiddleware;
+
 
 Router::add('GET', '/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
 
-Router::add('GET', '/', HomeController::class, 'index');
-Router::add('GET', '/hello', HomeController::class, 'hello', [AuthMiddleware::class]);
-Router::add('GET', '/world', HomeController::class, 'world', [AuthMiddleware::class]);
-Router::add('GET', '/about', HomeController::class, 'about');
+Router::add('GET', '/', HomeController::class, 'index', []);
 
 
 Router::run();
