522353d82ce2c7e46fc55a3b585da7910ce7939c
AriSyafri <diamondThink15@gmail.com>
2024-12-14 09:54:10 +0700

    menambah view index halaman awal

--- app/View/Home/index.php
+++ app/View/Home/index.php
@@ -2,7 +2,7 @@
     <div class="row align-items-center g-lg-5 py-5">
         <div class="col-lg-7 text-center text-lg-start">
             <h1 class="display-4 fw-bold lh-1 mb-3">Pengelolaan Inventori Toko Atk</h1>
-            <p class="col-lg-10 fs-4">by <a target="_blank" href="#">By Ari Syafri</a></p>
+            <p class="col-lg-10 fs-4">by <a target="_blank" href="#">Ari Syafri</a></p>
         </div>
         <div class="col-md-10 mx-auto col-lg-5">
             <div class="p-4 p-md-5 border rounded-3 bg-light">
