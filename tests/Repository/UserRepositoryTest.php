b39846fcbcecfb70f13c50136209eb2d2189b812
AriSyafri <diamondThink15@gmail.com>
2024-12-14 10:56:27 +0700

    menambah unit test user repository

---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dots\Toko\Atk\Repository;
+
+use Dots\Toko\Atk\Config\Database;
+use Dots\Toko\Atk\Domain\User;
+use PHPUnit\Framework\TestCase;
+
+class UserRepositoryTest extends TestCase
+{
+    private UserRepository $userRepository;
+
+    protected function setUp(): void
+    {
+        $this->userRepository = new UserRepository(Database::getConnection());
+        $this->userRepository->deleteAll();
+    }
+
+    public function testSaveSuccess()
+    {
+        $user = new User();
+        $user->id = "ari";
+        $user->name = "Ari";
+        $user->password = "rahasia";
+
+        $this->userRepository->save($user);
+        
+        $result = $this->userRepository->findById($user->id);
+        self::assertEquals($user->id, $result->id);
+        self::assertEquals($user->name, $result->name);
+        self::assertEquals($user->password, $result->password);
+        
+    }
+
+    public function testFindByIdNotFound()
+    {
+        $user = $this->userRepository->findById("notFound");
+        self::assertNull($user);
+    }
+
+}
