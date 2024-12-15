6933a52a24bd1aa522ca691a8d13876f7bb4b609
AriSyafri <diamondThink15@gmail.com>
2024-12-15 22:57:58 +0700

    memuat fitur user registration controller

---
+++ app/Controller/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Dots\Toko\Atk\Controller;
+
+use Dots\Toko\Atk\App\View;
+use Dots\Toko\Atk\Config\Database;
+use Dots\Toko\Atk\Exception\ValidationException;
+use Dots\Toko\Atk\Model\UserRegisterRequest;
+use Dots\Toko\Atk\Repository\UserRepository;
+use Dots\Toko\Atk\Service\UserService;
+
+class UserController
+{
+
+    private UserService $userService;
+
+    public function __construct()
+    {
+        $connection = Database::getConnection();
+        $userRepository = new UserRepository($connection);
+        $this->userService = new UserService($userRepository);
+    } 
+
+    
+    public function register(){
+        View::render('User/register', [
+            'title' => 'Register new user'
+        ]);
+    }
+
+    public function postRegister(){
+        $request = new UserRegisterRequest();
+        $request->id = $_POST['id'];
+        $request->name = $_POST['name'];
+        $request->password = $_POST['password'];
+
+        try {
+            $this->userService->register($request);
+            // redirect to users/login
+            View::redirect('/users/login');
+        } catch (ValidationException $exception) {
+            View::render('User/register', [
+                'title' => 'Register new user',
+                'error' => $exception->getMessage()
+            ]);
+        }
+
+
+    }
+}
--- public/index.php
+++ public/index.php
@@ -5,9 +5,19 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dots\Toko\Atk\App\Router;
 use Dots\Toko\Atk\Controller\HomeController;
+use Dots\Toko\Atk\Controller\UserController;
+use Dots\Toko\Atk\Config\Database;
+
+Database::getConnection('prod');
 
 
+// home controller
 Router::add('GET', '/', HomeController::class, 'index', []);
+
+// user controller
+Router::add('GET', '/users/register', UserController::class, 'register', []);
+Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
+
 
 Router::run();
 
