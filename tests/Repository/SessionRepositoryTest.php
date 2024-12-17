5ff7edc6c7992a8684554a65e662cba97b1c9851
AriSyafri <diamondThink15@gmail.com>
2024-12-17 11:14:41 +0700

    menambah hapus session repos dikarenakan foreign key

--- tests/Repository/SessionRepositoryTest.php
+++ tests/Repository/SessionRepositoryTest.php
@@ -4,24 +4,34 @@ namespace Dots\Toko\Atk\Repository;
 
 use Dots\Toko\Atk\Config\Database;
 use Dots\Toko\Atk\Domain\Session;
+use Dots\Toko\Atk\Domain\User;
 use PHPUnit\Framework\TestCase;
 
 class SessionRepositoryTest extends TestCase
 {
     private SessionRepository $sessionRepository;
+    private UserRepository $userRepository;
 
     protected function setUp():void 
     {
+        $this->userRepository = new UserRepository(Database::getConnection());
         $this->sessionRepository = new SessionRepository(Database::getConnection());
 
         $this->sessionRepository->deleteAll();
+        $this->userRepository->deleteAll();
+
+        $user = new User();
+        $user->id = "ari";
+        $user->name = "Ari";
+        $user->password = "rahasia";
+        $this->userRepository->save($user);
     }
 
     public function testSaveSuccess()
     {
         $session = new Session();
         $session->id = uniqid();
-        $session->userId = "budi";
+        $session->userId = "ari";
 
         $this->sessionRepository->save($session);
 
